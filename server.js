170f857bc8d00003837768adf850b60638d1fcd0
Alex Muhich <feldtlaufer@gmail.com>
2020-04-13 14:36:36 -0500

    register and save users

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import User from './models/User';
 
 
 //init express app
@@ -39,12 +41,38 @@ app.post(
         check('email', 'Please enter a valid email').isEmail(),
         check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if(!errors.isEmpty()){
             return res.status(422).json({errors: errors.array() });
         }else{
-            return res.send(req.body);
+            const { name, email, password } = req.body;
+            try{
+                //check if user exists
+                let user = await User.findOne({ email: email });
+                if(user){
+                    return res.status(400).json({
+                        errors: [{msg: 'User already exists' }]
+                    });
+                }
+
+                //create a new user
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                //encrypt the password
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                //save to the db and return
+                await user.save();
+                res.send('User sucessfully registered');
+            }catch(error){
+                res.status(500).send('Server error');
+            }
         }
     }
 );
