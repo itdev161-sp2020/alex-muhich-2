194eabfad0e7b0c8513df85fe865c94441bdc2b5
Alex Muhich <feldtlaufer@gmail.com>
2020-04-13 13:40:01 -0500

    create the api endpoint

--- server.js
+++ server.js
@@ -7,10 +7,26 @@ const app = express();
 //connect db
 connectDatabase();
 
+//configure middleware
+app.use(express.json({ extended: false }));
+
 //api endpoint
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route Post api/users
+ * @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 //connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
