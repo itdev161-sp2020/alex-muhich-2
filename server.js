9d936c7825f69c4858fd336e64071e2eb931e4e5
Alex Muhich <feldtlaufer@gmail.com>
2020-04-23 12:14:22 -0500

    create an end point to get posts

--- server.js
+++ server.js
@@ -196,6 +196,19 @@ app.post(
         }
     }
 );
+/**
+ * @route GET api/posts
+ * @desc Get posts
+ */
+app.get('/api/posts', auth, async (req, res) => {
+    try{
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    }catch(error){
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
 
 //connection listener
 const port = 5000
