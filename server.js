1654d03bf31d1825eaa036499acc6ee83a799748
Alex Muhich <feldtlaufer@gmail.com>
2020-04-23 12:05:46 -0500

    create post endpoints

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import User from './models/User';
 import jwt from 'jsonwebtoken';
 import config from 'config';
 import auth from './middleware/auth';
+import Post from './models/Post';
 
 
 //init express app
@@ -149,6 +150,47 @@ app.post(
                 returnToken(user, res);
 
             }catch(error){
+                res.status(500).send('Server error');
+            }
+        }
+    }
+);
+
+//Post endpoints
+/**
+ * @route POST api/posts
+ * @desc Create post
+ */
+app.post(
+    '/api/posts',
+    [
+        auth,
+        [
+            check('title', 'Title text is required').not().isEmpty(),
+            check('body', 'Body text is required').not().isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            res.status(400).json({errors: errors.array()});
+        }else{
+            const {title, body} = req.body;
+            try{
+                //get the user who created the post
+                const user = await User.findById(req.user.id);
+                //create a new post
+                const post = new Post({
+                    user: user.id,
+                    title: title,
+                    body: body
+                });
+                //save to the db and return
+                await post.save();
+
+                res.json(post);
+            }catch(error){
+                console.error(error);
                 res.status(500).send('Server error');
             }
         }
